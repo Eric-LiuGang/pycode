62e2a9851393a37c379c527debda0d4eae53fc11
Eric-LiuGang <350149094@qq.com>
2019-07-17 09:50:31 +0800

    Create zip_dir.py

---
+++ zip_dir.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import zipfile
+
+def zip_dir(dirname,zipfilename):
+    filelist = []
+    if os.path.isfile(dirname):
+        filelist.append(dirname)
+    else :
+        for root, dirs, files in os.walk(dirname):
+            for dir in dirs:
+                filelist.append(os.path.join(root,dir))
+            for name in files:
+                filelist.append(os.path.join(root, name))
+
+    zf = zipfile.ZipFile(zipfilename, "w", zipfile.zlib.DEFLATED)
+    for tar in filelist:
+        arcname = tar[len(dirname):]
+        #print arcname
+        zf.write(tar,arcname)
+    zf.close()
+
+if __name__=='__main__':
+    zip_dir("/tmp","/root/test.zip")
